4a52d0d89044833cbece561abc2bc71825e2d0f6
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-10-16 19:25:18 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016192434) do
+ActiveRecord::Schema.define(version: 20171016192511) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
